72cf332667c5a4fc1f903b007dd2831f9065c7ec
jack upt <jhonbahama@gmail.com>
2019-12-12 22:06:13 -0800

    Updated CSS and HTML

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () { 
+    $("#submitCity").click(function () {
+        return getWeather();
+    });
+    
+});
+function getWeather() {
+    var city = $("#city").val();
+
+    if (city != '') {
+        
+
+
+    }else
+}
